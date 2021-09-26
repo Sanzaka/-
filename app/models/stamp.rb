adf4d870db870f332801c3027914b3afa6828b88
Sanzaka <equation0815@icloud.com>
2021-09-26 11:27:32 +0000

    [add]stamp機能を非同期通信化

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -3,16 +3,4 @@ class Stamp < ApplicationRecord
   belongs_to :group
   belongs_to :group_message
 
-  def stamp1_exists?
-    
-  end
-
-  def stamp2_exists?
-    
-  end
-
-  def stamp3_exists?
-    
-  end
-
 end
