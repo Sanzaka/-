dd494e6dce57d89c845377ce7e49730c4a915753
Sanzaka <equation0815@icloud.com>
2021-09-27 06:20:51 +0000

    [fix]画像の配置箇所を変更、レイアウト微調整

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -7,13 +7,7 @@ class StampsController < ApplicationController
     @stamp.user_id = current_user.id
     @stamp.group_id = params[:group_id]
     @stamp.group_message_id = params[:message_id]
-    if @stamp.save!
-      flash[:notice] = "スタンプを送信しました！"
-      # redirect_to group_path(params[:group_id])
-    else
-      flash[:alert] = "スタンプの送信に失敗しました！"
-      # redirect_to group_path(params[:group_id])
-    end
+    @stamp.save!
   end
 
   def destroy
@@ -21,8 +15,6 @@ class StampsController < ApplicationController
     @group = Group.find(params[:group_id])
     @stamp = Stamp.find_by(user_id: current_user, group_id: params[:group_id], group_message_id: params[:id], choose_stamp: params[:choose_stamp])
     @stamp.destroy
-    flash[:notice] = "スタンプを削除しました！"
-    # redirect_to group_path(params[:group_id])
   end
 
   private
